f9cdc7d0be70ab34abbb34a106a958f1704a0a7a
Viet Hoang <ngviethoang0212@gmail.com>
2017-12-27 22:03:17 +0700

    update db config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_a67be6b302a228f'),
+            'username' => env('DB_USERNAME', 'bcfbac9e397332'),
+            'password' => env('DB_PASSWORD', '7258c914'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
